eb3d02396e6e04eb0ccf36136f482bdc37b3fdbc
Claudia <leiaofalderran@gmail.com>
2022-01-13 23:37:01 -0500

    worked on banking app

---
+++ src/com/packages/Account.java
@@ -0,0 +1,11 @@
+package com.packages;
+
+import java.util.Scanner;
+
+public class Account {
+    public static void main(String[] args) {
+        System.out.println("Hello, Welcome to the ABC Bank!" +
+                " What would you like to do today?");
+
+    }
+}
