ccb5eec0fc06ce0acf853a299157692f95077061
Claudia <leiaofalderran@gmail.com>
2022-01-25 00:43:52 -0500

    began working on baking app

--- src/com/packages/Account.java
+++ src/com/packages/Account.java
@@ -5,7 +5,13 @@ import java.util.Scanner;
 public class Account {
     public static void main(String[] args) {
         System.out.println("Hello, Welcome to the ABC Bank!" +
-                " What would you like to do today?");
+                           "\n" + "What would you like to do today?" +"\n");
+        System.out.println("1. Check Balance " +
+                     "\n" + "2. Withdraw" + "\n" + "3. Deposit" + "\n" + "4. Exit");
+
+        Scanner scanner = new Scanner(System.in);
+        String userInput = scanner.nextLine();
+
 
     }
 }
